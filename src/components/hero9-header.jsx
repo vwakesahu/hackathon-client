533faefbd384cfa6aa7fde26ba99b7902737819a
Vivek Sahu <viveksahu1762@gmail.com>
2025-04-02 06:28:06 +0530

    explore app

--- src/components/hero9-header.jsx
+++ src/components/hero9-header.jsx
@@ -60,7 +60,7 @@ export const HeroHeader = () => {
             <div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
               <div className="flex w-full justify-end sm:justify-start">
                 <Button asChild size="sm">
-                  <Link href="/app">
+                  <Link href="/app/airdrop-eth">
                     <span>Explore App</span>
                   </Link>
                 </Button>
